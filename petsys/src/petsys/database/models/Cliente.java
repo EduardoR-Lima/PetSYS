ee970022cd7583af57353bb84be69bbffe97ecbf
Marcos Paiva <marcos_paulino2008@hotmail.com>
2024-08-30 11:52:39 -0300

    atualizacao do codigo

--- petsys/src/petsys/database/models/Cliente.java
+++ petsys/src/petsys/database/models/Cliente.java
@@ -1,8 +1,74 @@
 package petsys.database.models;
 
 public class Cliente {
-  private int id;
-  private String cpf;
-  private String nome;
-  private String endereco;
+	private int id;
+	private String cpf;
+	private String nome;
+	private String endereco;
+	private String telefoneCel;
+	private String email;
+	private String dataAdicao;
+	
+	public Cliente() {
+	}
+	
+	public Cliente(int id, String cpf, String nome, String endereco, String telefoneCel, String email, String dataAdicao) {
+		this.id = id;
+		this.cpf = cpf;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.telefoneCel = telefoneCel;
+		this.email = email;
+		this.dataAdicao = dataAdicao;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", cpf=" + cpf + ", nome=" + nome + ", endereco=" + endereco + ", telefoneCel="
+				+ telefoneCel + ", email=" + email + ", dataAdicao=" + dataAdicao + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefoneCel() {
+		return telefoneCel;
+	}
+	public void setTelefoneCel(String telefoneCel) {
+		this.telefoneCel = telefoneCel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDataAdicao() {
+		return dataAdicao;
+	}
+	public void setDataAdicao(String dataAdicao) {
+		this.dataAdicao = dataAdicao;
+	}
+	
 }
--- petsys/src/petsys/database/models/Veterinario.java
+++ petsys/src/petsys/database/models/Veterinario.java
@@ -1,5 +1,74 @@
 package petsys.database.models;
 
 public class Veterinario {
+	private int id;
+	private String crmv;
+	private String nome;
+	private String endereco;
+	private String telefoneCel;
+	private String email;
+	private String dataAdicao;
 
+	public Veterinario () {
+	}
+		public Veterinario(int id, String crmv, String nome, String endereco, String telefoneCel, String email, String dataAdicao) {
+			this.id = id;
+			this.crmv = crmv;
+			this.nome = nome;
+			this.endereco = endereco;
+			this.telefoneCel = telefoneCel;
+			this.email = email;
+			this.dataAdicao = dataAdicao;
+	}
+		@Override
+		public String toString() {
+			return "Veterinario [id=" + id + ", crmv=" + crmv + ", nome=" + nome + ", endereco=" + endereco
+					+ ", telefoneCel=" + telefoneCel + ", email=" + email + ", dataAdicao=" + dataAdicao
+					+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+					+ "]";
+		}
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getCrmv() {
+			return crmv;
+		}
+		public void setCrmv(String crmv) {
+			this.crmv = crmv;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getEndereco() {
+			return endereco;
+		}
+		public void setEndereco(String endereco) {
+			this.endereco = endereco;
+		}
+		public String getTelefoneCel() {
+			return telefoneCel;
+		}
+		public void setTelefoneCel(String telefoneCel) {
+			this.telefoneCel = telefoneCel;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getDataAdicao() {
+			return dataAdicao;
+		}
+		public void setDataAdicao(String dataAdicao) {
+			this.dataAdicao = dataAdicao;
+		}
+		
 }
+
